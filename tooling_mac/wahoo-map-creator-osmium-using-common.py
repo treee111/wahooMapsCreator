c4ac1697412bd048a493d533e73b365862d9c030
Benjamin <benni.kreuscher@gmail.com>
2021-06-05 22:27:10 +0200

    move getRegion to common

--- tooling_mac/wahoo-map-creator-osmium-using-common.py
+++ tooling_mac/wahoo-map-creator-osmium-using-common.py
@@ -3,14 +3,45 @@
 # import official python packages
 import glob
 import json
+import multiprocessing
 import os
+# import os.path
+
 import subprocess
 import sys
+
 
 # import custom python packages
 from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from common_resources import file_directory_functions
+from common_resources import osm_maps_functions
+
+########### Configurable Parameters
+
+# Maximum age of source maps or land shape files before they are redownloaded
+Max_Days_Old = 14
+
+# Force (re)processing of source maps and the land shape file
+# If 0 use Max_Days_Old to check for expired maps
+# If 1 force redownloading/processing of maps and landshape 
+Force_Processing = 0
+
+# Save uncompressed maps for Cruiser
+Save_Cruiser = 0
+
+# Number of threads to use in the mapwriter plug-in
+threads = str(multiprocessing.cpu_count() - 1)
+if int(threads) < 1:
+    threads = 1
+# Or set it manually to:
+#threads = 1
+#print(f'threads = {threads}/n')
+
+# Number of workers for the Osmosis read binary fast function
+workers = '1'
+
+########### End of Configurable Parameters
 
 # logging
 # # means top-level command
@@ -34,6 +65,9 @@ if len(sys.argv) != 2:
     print(f'! Usage: {sys.argv[0]} Country name part of a .json file.')
     sys.exit()
 
+# if osm_maps_functions.getRegionOfCountry(sys.argv[1]) == '' :
+#     print ('Invalid country name.')
+#     sys.exit()
 
 print('\n\n# Read json file')
 with open(sys.argv[1]) as f:
