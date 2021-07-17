cd51690824ee52fb2bf3b43caf2799bdfc53875c
Benjamin <benni.kreuscher@gmail.com>
2021-07-17 11:17:52 +0200

    input gui and cli
    - first draft

---
+++ common_resources/input.py
@@ -0,0 +1,263 @@
+"""
+functions and object for processing input via CLI and GUI
+"""
+#!/usr/bin/python
+
+# import official python packages
+import argparse
+import ast
+import sys
+
+# for gui
+import tkinter as tk
+from tkinter import ttk
+
+# import custom python packages
+from common_resources import constants
+
+class InputData():
+    """
+    object with all parameters to process maps
+    """
+    def __init__(self):
+        self.region = ""
+        self.country = ""
+        self.max_days_old = 14
+
+        self.force_download = False
+        self.force_processing = False
+        self.border_countries = False
+        self.save_cruiser = False
+
+        self.tag_wahoo_xml = "tag-wahoo.xml"
+
+class Input(tk.Tk):
+    """
+    This is the class to proces user-input via CLI and GUI
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.region = ""
+        self.country = ""
+
+        self.o_input_data = InputData()
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # self.geometry("420x360")
+        self.title("Wahoo map creator")
+        self.configure(bg="white")
+        self.option_add("*Font", "Calibri 16")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill = "both", expand = True)
+
+        self.first = ComboboxesEntryField(container, self.o_input_data.max_days_old)
+        self.first.pack(side="top", fill="x")
+
+        self.third = Checkbuttons(container, self.o_input_data, controller = self)
+        self.third.pack(side="top", fill="x")
+
+        self.four = Buttons(container, controller = self)
+        self.four.pack(side="top", fill="x")
+
+    def start_gui(self):
+        """
+        start GUI
+        """
+        # start GUI
+        self.mainloop()
+
+        if self.o_input_data.country == "none" or self.o_input_data.country == " ":
+            sys.exit()
+        else:
+            return self.o_input_data
+
+
+    def handle_create_map(self):
+        """
+        run when Button "Create" is pressed
+        """
+        self.o_input_data.region = self.first.cb_continent.get().replace("-", "")
+        self.o_input_data.country = self.first.cb_country.get()
+        self.o_input_data.max_days_old = int(self.first.input_maxdays.get())
+
+        self.o_input_data.force_download = self.third.checkb_download.get()
+        self.o_input_data.force_processing = self.third.checkb_processing.get()
+        self.o_input_data.border_countries = self.third.checkb_border_countries.get()
+        self.o_input_data.save_cruiser = self.third.checkb_save_cruiser.get()
+
+        self.destroy()
+
+
+    def switch_reload(self):
+        """
+        switch edit-mode of max-days field
+        """
+        if self.first.en_max_days_old['state'] == tk.NORMAL:
+            self.first.en_max_days_old.configure(state=tk.DISABLED)
+        else:
+            self.first.en_max_days_old.configure(state=tk.NORMAL)
+
+
+    def cli_arguments(self):
+        """
+        process CLI arguments
+        """
+
+        o_input_data = InputData()
+
+        # input argument creation and processing
+        desc = "Create up-to-date maps for your Wahoo ELEMNT and Wahoo ELEMNT BOLT"
+        parser = argparse.ArgumentParser(description=desc)
+
+        # country or file to create maps for
+        parser.add_argument("country", help="country to generate maps for")
+        # Maximum age of source maps or land shape files before they are redownloaded
+        parser.add_argument('-md', '--maxdays', type=int, default=o_input_data.max_days_old,
+                            help="maximum age of source maps and other files")
+        # Calculate also border countries of input country or not
+        parser.add_argument('-bc', '--bordercountries', action='store_true',
+                            help="process whole tiles which involve border countries")
+        # Force download of source maps and the land shape file
+        # If False use Max_Days_Old to check for expired maps
+        # If True force redownloading of maps and landshape
+        parser.add_argument('-fd', '--forcedownload', action='store_true',
+                            help="force download of files")
+        # Force (re)processing of source maps and the land shape file
+        # If False only process files if not existing
+        # If True force processing of files
+        parser.add_argument('-fp', '--forceprocessing', action='store_true',
+                            help="force processing of files")
+        # Save uncompressed maps for Cruiser if True
+        parser.add_argument('-c', '--cruiser', action='store_true',
+                            help="save uncompressed maps for Cruiser")
+        # specify the file with tags to keep in the output // file needs to be in common_resources
+        parser.add_argument('-tag', '--tag_wahoo_xml', default=self.o_input_data.tag_wahoo_xml,
+                            help="file with tags to keep in the output")
+
+
+        # set instance-attributes of class
+        try:
+            args = parser.parse_args()
+
+            o_input_data = InputData()
+            o_input_data.country = args.country
+            o_input_data.max_days_old = args.maxdays
+
+            o_input_data.force_download = args.forcedownload
+            o_input_data.force_processing = args.forceprocessing
+            o_input_data.border_countries = args.bordercountries
+            o_input_data.save_cruiser = args.tag_wahoo_xml
+
+            return o_input_data
+
+        except SystemExit:
+            return False
+
+
+class ComboboxesEntryField(tk.Frame):
+    """
+    Comboboxes and Entry-Field for max days
+    """
+    def __init__(self, parent, default_max_days_old):
+        tk.Frame.__init__(self, parent)
+
+        # Labels
+        self.lab_top = tk.Label(self, text="Select continent and country to create a map")
+        self.lab_continent = tk.Label(self, text="Select continent:")
+        self.lab_country = tk.Label(self, text='Select country:')
+
+        # Comboboxes
+        self.cb_continent = ttk.Combobox(self, values=constants.continents, state="readonly")
+        self.cb_continent.current(0) # pre-select first entry in combobox
+        self.cb_continent.bind("<<ComboboxSelected>>", self.callback_continent)
+
+        self.cb_country = ttk.Combobox(self, values=constants.europe, state = "readonly")
+        # cboCountry.current(0)
+
+        # Positioning
+        self.lab_top.grid(column = 0, row = 0, columnspan = 2, padx = 5, pady = 10)
+
+        self.lab_continent.grid(column = 0, row = 1, sticky = tk.E, padx = 5, pady = 2)
+        self.cb_continent.grid(column = 1, row = 1, sticky = tk.W, padx = 10, pady = 2)
+
+        self.lab_country.grid(column = 0, row = 2, sticky = tk.E, padx = 5, pady = 2)
+        self.cb_country.grid(column = 1, row = 2, sticky = tk.W, padx = 10, pady = 2)
+
+        self.lab_max_days_old = tk.Label(self, text='Max Old Days:')
+
+        self.input_maxdays = tk.StringVar()
+        self.input_maxdays.set(str(default_max_days_old))
+
+        self.en_max_days_old = tk.Entry(self, textvar = self.input_maxdays, width = 5)
+
+        self.lab_max_days_old.grid(column = 0, row = 3, sticky = tk.E, padx = 5, pady = 2)
+        self.en_max_days_old.grid(column = 1, row = 3, sticky = tk.W, padx = 10)
+
+
+    def callback_continent(self):
+        """
+        set value-list of countries after changing continent
+        """
+        continent = self.cb_continent.get()
+        # get countries for selected region and set for combobox
+        self.cb_country["values"] = ast.literal_eval('constants.' + continent.replace("-", ""))
+        self.cb_country.current(0)
+
+
+class Checkbuttons(tk.Frame):
+    """
+    Checkbuttons for GUI
+    """
+    def __init__(self, parent, oInputData, controller):
+        tk.Frame.__init__(self, parent) #, bg="red"
+        self.controller = controller
+
+        self.checkb_download = tk.BooleanVar()
+        self.checkb_download.set(oInputData.force_download)
+
+        self.chk_force_download = tk.Checkbutton(self, text = "Force download",
+                            var = self.checkb_download)
+        self.chk_force_download.bind("<Button-1>", self.controller.switch_reload)
+
+
+        self.checkb_processing = tk.BooleanVar()
+        self.checkb_processing.set(oInputData.force_processing)
+
+        self.checkb_border_countries = tk.BooleanVar()
+        self.checkb_border_countries.set(oInputData.border_countries)
+
+        self.checkb_save_cruiser = tk.BooleanVar()
+        self.checkb_save_cruiser.set(oInputData.save_cruiser)
+
+        self.chk_force_processing = tk.Checkbutton(self, text = "Force processing",
+                                var = self.checkb_processing)
+
+        self.chk_border_countries = tk.Checkbutton(self, text = "Process border countries",
+                                var = self.checkb_border_countries)
+
+        self.chk_save_cruiser = tk.Checkbutton(self, text = "Save uncompressed maps for Cruiser",
+                                var = self.checkb_save_cruiser)
+
+        self.chk_force_download.grid(column = 0, row = 0, sticky = tk.W, padx = 15, pady = 5)
+        self.chk_force_processing.grid(column = 0, row = 1, sticky = tk.W, padx = 15, pady = 5)
+        self.chk_border_countries.grid(column = 0, row = 2, sticky = tk.W, padx = 15, pady = 5)
+        self.chk_save_cruiser.grid(column = 0, row = 3, columnspan = 2, sticky = tk.W,
+                                        padx = 15, pady = 5)
+
+
+class Buttons(tk.Frame):
+    """
+    Buttons for GUI
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg="light grey")
+        self.controller = controller
+        self.btn_ok = tk.Button(self, text = "Create map", width = 15)
+        self.btn_ok.bind("<Button-1>", self.controller.handle_create_map)
+
+        self.btn_cancel = tk.Button(self, text="Exit", width = 15, command = parent.destroy)
+
+        self.btn_ok.pack(side = "left")
+        self.btn_cancel.pack(side = "right")
--- wahoo_map_creator.py
+++ wahoo_map_creator.py
@@ -3,58 +3,35 @@ executable file to create up-to-date map-files for the Wahoo ELEMNT and Wahoo EL
 """
 #!/usr/bin/python
 
-# import official python packages
-import argparse
-
 # import custom python packages
 from common_resources.osm_maps_functions import OsmMaps
+# import common_resources.input_functions as in_fct
+from common_resources.input import Input
 
 # logging used in the terminal output:
 # # means top-level command
 # ! means error
 # + means additional comment in a working-unit
 
+# if __name__ == "__main__":
+# root = tk.Tk()
+# MainApplication(root).pack(side="top", fill="both", expand=True)
+# root.mainloop()
+# window = tk.Tk()
+oInput = Input()
+# oInput.pack(side="top", fill="both", expand=True)
 
-# input argument creation and processing
-DESC = "Create up-to-date maps for your Wahoo ELEMNT and Wahoo ELEMNT BOLT"
-parser = argparse.ArgumentParser(description=DESC)
+oInputData = oInput.cli_arguments()
+if oInputData is False:
+    oInputData = oInput.start_gui()
 
-# country or file to create maps for
-parser.add_argument("country", help="country to generate maps for")
-# Maximum age of source maps or land shape files before they are redownloaded
-parser.add_argument('-md', '--maxdays', type=int, default=14,
-                    help="maximum age of source maps and other files")
-# Calculate also border countries of input country or not
-parser.add_argument('-bc', '--bordercountries', action='store_true',
-                    help="process whole tiles which involve border countries")
-# Force download of source maps and the land shape file
-# If False use Max_Days_Old to check for expired maps
-# If True force redownloading of maps and landshape
-parser.add_argument('-fd', '--forcedownload', action='store_true',
-                    help="force download of files")
-# Force (re)processing of source maps and the land shape file
-# If False only process files if not existing
-# If True force processing of files
-parser.add_argument('-fp', '--forceprocessing', action='store_true',
-                    help="force processing of files")
-# Save uncompressed maps for Cruiser if True
-parser.add_argument('-c', '--cruiser', action='store_true',
-                    help="save uncompressed maps for Cruiser")
-# specify the file with tags to keep in the output // file needs to be in common_resources
-parser.add_argument('-tag', '--tag_wahoo_xml', default="tag-wahoo.xml",
-                    help="file with tags to keep in the output")
-
-# get all entered arguments
-args = parser.parse_args()
-
-
-oOSMmaps = OsmMaps(args.forceprocessing)
+oOSMmaps = OsmMaps(oInputData.force_processing)
 
 # Read json file
 # Check for expired land polygons file and download, if too old
 # Check for expired .osm.pbf files and download, if too old
-oOSMmaps.process_input(args.country, args.bordercountries)
-oOSMmaps.check_and_download_files(args.maxdays, args.forcedownload)
+oOSMmaps.process_input(oInputData.country, oInputData.border_countries)
+oOSMmaps.check_and_download_files(oInputData.max_days_old, oInputData.force_download)
 
 # Filter tags from country osm.pbf files'
 oOSMmaps.filter_tags_from_country_osm_pbf_files()
@@ -72,11 +49,11 @@ oOSMmaps.split_filtered_country_files_to_tiles()
 oOSMmaps.merge_splitted_tiles_with_land_and_sea()
 
 # Creating .map files
-oOSMmaps.create_map_files(args.cruiser, args.tag_wahoo_xml)
+oOSMmaps.create_map_files(oInputData.save_cruiser, oInputData.tag_wahoo_xml)
 
 # Zip .map.lzma files
 oOSMmaps.zip_map_files()
 
 # Make Cruiser map files zip file
-if args.cruiser is True:
+if oInputData.save_cruiser is True:
     oOSMmaps.make_cruiser_files()
