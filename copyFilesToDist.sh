c3cd530767931d7246cc616573cf646923846e82
Benjamin K <53038537+treee111@users.noreply.github.com>
2022-02-23 22:42:58 +0100

    [FEATURE] CI/CD: create OS-specific .zip files during build for release .zip files, separate cruiser.zip Asset and delete unused files (#90)
    * move files and delete unused
    
    - move files from /tooling to /common_resources
    - delete unused shape2osm folder and mappack because these are not used
    
    * create (build) OS-specific .zip files
    
    * delete older conda ...-mac.yml
    
    * Do not include top-level folder in .zip files
    
    - by cd'ing into each OS's folder
    
    * make cruiser files a separate .zip
    
    - incl. deletion of line which does nothing

--- copyFilesToDist.sh
+++ copyFilesToDist.sh
@@ -1,37 +1,56 @@
-FOLDER_NAME="wahooMapsCreator-"${GITHUB_REF_VARIABLE}
-echo "Foldername: ${FOLDER_NAME}"
+FOLDER_NAME_MAC="wahooMapsCreator-"${GITHUB_REF_VARIABLE}"-macOS-Linux"
+FOLDER_NAME_WIN="wahooMapsCreator-"${GITHUB_REF_VARIABLE}"-Windows"
+FOLDER_NAME_CRUISER="cruiser-mapviewer-"${GITHUB_REF_VARIABLE}"-macOS-Linux-Windows"
+
 echo "Github-Ref: ${GITHUB_REF_VARIABLE}"
 
+# delete folder with content and create top-level folder
 rm -rf dist
-
-# create top-folders and move into given foldername-folder
 mkdir -p dist
-mkdir -p dist/${FOLDER_NAME}
-cd dist/${FOLDER_NAME}
+# create OS-specific folders
+mkdir -p dist/${FOLDER_NAME_MAC}
+mkdir -p dist/${FOLDER_NAME_WIN}
+
+# zip cruiser stuff
+cd tooling/cruiser
+zip -r ../../dist/${FOLDER_NAME_CRUISER}.zip *
+
+# navigate into mac folder
+cd ../..
+cd dist/${FOLDER_NAME_MAC}
+
+# create empty sub-folders
 mkdir -p ./common_python
 mkdir -p ./common_resources
 mkdir -p ./conda_env
+mkdir -p ./tooling
 
 # copy files into dist-folder
 cp -a ../../common_python/*.py ./common_python/
+
 cp -a ../../common_resources/*.xml ./common_resources/
-# cp -a ../../common_resources/*.osm ./common_resources/
 cp -a ../../common_resources/json/ ./common_resources/json
 cp -a ../../common_resources/tag_wahoo_adjusted/ ./common_resources/tag_wahoo_adjusted
 cp -a ../../common_resources/tag_wahoo_initial/ ./common_resources/tag_wahoo_initial
+
 cp -a ../../docs ./docs
-cp -a ../../tooling ./tooling
-cp -a ../../tooling_windows ./tooling_windows
 cp -a ../../wahoo_map_creator.py ../../CHANGELOG.md ../../README.md ./
 
 # file for virtual environments / legacy pip install
 cp -a ../../conda_env/enduser.yml ./conda_env/
 cp -a ../../conda_env/requirements.txt ./conda_env/
 
-# zip content into .zip file
-zip -r ../${FOLDER_NAME}.zip *
-# echo "dist/${FOLDER_NAME}/: ls"
-# ls
-# cd ..
-# echo "dist/: ls"
-# ls
+# navigate one dir up into "dist" folder and copy content from Mac to Win
+cd ..
+cp -R ${FOLDER_NAME_MAC}/* ${FOLDER_NAME_WIN}
+
+# add Win specific stuff
+cp -a ../tooling_windows ./${FOLDER_NAME_WIN}/tooling_windows
+
+# zip content into .zip file - by cd'ing without including root folder
+cd ${FOLDER_NAME_MAC}
+zip -r ../${FOLDER_NAME_MAC}.zip *
+
+cd ..
+cd ${FOLDER_NAME_WIN}
+zip -r ../${FOLDER_NAME_WIN}.zip *
