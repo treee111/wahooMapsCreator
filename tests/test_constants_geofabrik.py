fab8f8aed433d5a400e5b1159e58e03855ce2336
Benjamin Kreuscher <benni.kreuscher@gmail.com>
2023-02-15 22:09:37 +0100

    unittest: compare built URL with geofabrik URL

--- tests/test_constants_geofabrik.py
+++ tests/test_constants_geofabrik.py
@@ -7,6 +7,7 @@ import unittest
 # import custom python packages
 from wahoomc import file_directory_functions as fd_fct
 from wahoomc.downloader import Downloader
+from wahoomc.downloader import get_osm_pbf_filepath_url
 from wahoomc import constants
 from wahoomc.constants_functions import GeofabrikJson
 
@@ -72,6 +73,74 @@ class TestConstantsGeofabrik(unittest.TestCase):
 
         self.assertCountEqual(geofabrik_regions_w_russia,
                               id_with_no_parent_geofabrik)
+
+    def test_gofabrik_url_against_built_url(self):
+        """
+        go through all files in the wahoo_mc/resources/json directory
+        - compare the built URL via existing coding with the url out of the geofabrik json file
+        - some countries are skipped because they do not exist in geofabrik
+        - in addition
+            - skip countries if there is a geofabrik URL (because this URL is different to the built one)
+            - skip countries which do not have a direct geofabrik json entry, mostly they are in another country/region
+        """
+        for country in self.relevant_countries:
+            built_url = None
+            geofabrik_url = None
+
+            # build URL with existing coding
+            try:
+                map_file_path, built_url = get_osm_pbf_filepath_url(country)
+            except SystemExit:
+                pass
+
+            # grab URL from geofabrik json
+            geofabrik_url = self.o_geofabrik_json.find_geofbrik_url(country)
+
+            if not geofabrik_url:
+                # check with replaced _ by -
+                geofabrik_url = self.o_geofabrik_json.find_geofbrik_url(
+                    country.replace('_', '-'))
+                if not geofabrik_url:
+                    # check with replaced _ by - AND prefix us/
+                    geofabrik_url = self.o_geofabrik_json.find_geofbrik_url(
+                        'us/'+country.replace('_', '-'))
+
+            skip_countries_built_url_is_false = [
+                'papua_new_guinea', 'east_timor',
+                'antarctica',  # built url is false
+                'georgia'  # will issue north-america/us but europe is right. Reason: order in get_geofabrik_region_of_country
+            ]
+
+            # skip_countries_for_url_comparison = [
+            #     'united_arab_emirates', 'kuwait', 'qatar', 'bahrain', 'saudi_arabia', 'oman',  # gcc-states
+            #     'malaysia', 'singapore', 'brunei',  # malaysia-singapore-brunei
+            #     'christmas_island',  # indonesia
+            #     'paracel_islands', 'spratly_islands', 'british_indian_ocean_territory',  # asia
+            #     'macao', 'hong_kong',  # china
+            #     'palestina', 'israel',  # israel-and-palestine
+            #     'northern_mariana_islands', 'cocos_islands', 'commonwealth_of_the_northern_mariana_islands', 'american_samoa',  # australia
+            #     'antarctica',  # built url is false
+            #     'georgia',  # will issue north-america/us but europe is right because of the order in get_geofabrik_region_of_country
+            #     'united_kingdom', 'åland', 'gibraltar', 'san_marino', 'san_marino', 'vatican_city', 'ireland',  # europe
+            #     'bosnia_and_herzegovina', 'jersey', 'guernsey', 'svalbard_and_jan_mayen',  # europe
+            #     'saint_helena', 'cote_d_ivoire', 'western_sahara', 'democratic_republic_of_the_congo']  # africa
+
+            if built_url != geofabrik_url:
+                # # either it is a country to skip
+                # if (country in skip_countries_for_url_comparison_if_geofabrik_url and geofabrik_url) \
+                #         or country in skip_countries_for_url_comparison:
+                #     continue
+
+                # or there is no built url but a geofabrik one - ok!
+                # for some countries the built url is false
+                if built_url is None or \
+                        country in skip_countries_built_url_is_false:
+                    self.assertTrue(geofabrik_url, msg='country: '+country)
+                    continue
+
+            # "normal" compare, also if both are empty/ None
+            self.assertEqual(built_url, geofabrik_url,
+                             msg='country: '+country)
 
     def get_geofabrik_id_by_json_file_country(self, country):
         """
