c9d0c348efac9226a2a2eec4cb5c1f073e4ada91
Benjamin Kreuscher <benni.kreuscher@gmail.com>
2023-02-18 11:02:15 +0100

    after running vulture

--- wahoomc/constants.py
+++ wahoomc/constants.py
@@ -24,6 +24,7 @@ OSMOSIS_WIN_FILE_PATH = os.path.join(
 WAHOO_MC_DIR = os.path.dirname(__file__)
 RESOURCES_DIR = os.path.join(WAHOO_MC_DIR, 'resources')
 TOOLING_WIN_DIR = os.path.join(WAHOO_MC_DIR, 'tooling_win')
+# location of repo / python installation - not used
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 VERSION = '3.2.0'
 
