ef4fbbee67db6f07165cca09470eb4ec954ee2ef
Benjamin Kreuscher <benni.kreuscher@gmail.com>
2025-04-22 11:25:50 +0200

    restructure to not have a IndexError
    - IndexError occurred when map file was not downloaded already and hence the log.debug printed a not-existing part of the array/dict/list

--- wahoomc/downloader.py
+++ wahoomc/downloader.py
@@ -331,10 +331,11 @@ class Downloader:
 
         for country in self.border_countries:
             # check for already existing .osm.pbf file
-            map_file_path = glob.glob(
-                build_osm_pbf_filepath(country))
+            expected_map_location = build_osm_pbf_filepath(country)
             log.debug(
-                ' Checking for map at filepath: %s',map_file_path[0])
+                ' Checking for map at filepath: %s',expected_map_location)
+            map_file_path = glob.glob(expected_map_location)
+
             if len(map_file_path) != 1:
                 map_file_path = glob.glob(
                     f'{USER_MAPS_DIR}/**/{country}-latest.osm.pbf')
